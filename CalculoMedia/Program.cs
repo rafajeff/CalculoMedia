850d8262483fcc080f9e2b617ec233b5557c1cac
rafael jeferson <rafajeferson@hotmail.com>
2019-09-21 23:19:34 -0300

    refatoração e mudança de frases

--- CalculoMedia/Program.cs
+++ CalculoMedia/Program.cs
@@ -9,23 +9,25 @@ namespace CalculoMedia
             decimal   n1, n2, media = 0;
             Console.WriteLine("Digite o primeiro valor");
             n1 = decimal.Parse(Console.ReadLine());
+
             Console.WriteLine("Digite o segunda valor");
             n2 = decimal.Parse(Console.ReadLine());
-            media =( (n1 + n2) / 2);
+
+            media = (n1 + n2) / 2;
+
             if (media >= 7)
             {
-                Console.WriteLine( $"Parabens você fez 65. Sua média foi: {media}");
-
+                Console.WriteLine( $"Parabens você foi aprovado. Sua média foi: {media}");
             }
             else
             {
                 if (media > 5 && media < 7)
                 {
-                    Console.WriteLine($"Recuperation. Sua média: {media}");
+                    Console.WriteLine($"Você está de recuperação. Sua média: {media}");
                 }
                 else
                 {
-                    Console.WriteLine($"Se fodeu!!! Sua media foi: {media}");
+                    Console.WriteLine($"Você está reprovado Sua media foi: {media}");
                 }
             }
         }
